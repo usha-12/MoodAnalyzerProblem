3e25d5acbed4934ee28ddc642b8718f5eef98ada
usha-12 <usha12ahirwar@gmail.com>
2022-11-09 22:10:39 +0530

    respond Happy or Sad Mood

--- src/main/java/com/brideglabz/exception/MoodAnalyser.java
+++ src/main/java/com/brideglabz/exception/MoodAnalyser.java
@@ -1,2 +1,21 @@
-package com.brideglabz.exception;public class MoodAnalyser {
-}
+package com.brideglabz.exception;
+
+public class MoodAnalyser {
+    public static String analyseMood(String message) {
+        if (message.toLowerCase().contains("sad")) {
+            return "Sad";
+        } else if (message.toLowerCase().contains("happy")) {
+            return "Happy";
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String mood = MoodAnalyser.analyseMood("User is Happy");
+        System.out.println(mood);
+        mood = MoodAnalyser.analyseMood("User is Sad");
+        System.out.println(mood);
+    }
+
+    }
+//
