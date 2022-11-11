ae9e829e701ea61c0db98db2e6674b4e7f345a87
usha-12 <usha12ahirwar@gmail.com>
2022-11-11 14:55:27 +0530

    Custom Exception MoodAnalysisException

--- src/main/java/com/brideglabz/exception/MoodAnalyser.java
+++ src/main/java/com/brideglabz/exception/MoodAnalyser.java
@@ -1,24 +1,27 @@
 package com.brideglabz.exception;
 public class MoodAnalyser {
     public static String message;
-    public MoodAnalyser(String message) {
-        this.message = message;
-        analyseMood();
-    }public String getMessage() {
-        return message;
-    }
+
     public void setMessage(String message) {
+
         this.message = message;
     }
-    public static String analyseMood() {
+    public  MoodAnalyser() {
+        this.message = null;
+
+    }
+
+    public String analyseMood() throws MoodAnalyserException {
         try {
-            if (message.toLowerCase().contains("sad")) {
-                return "Sad";
-            } else {
-                return "Happy";
-            }
-        } catch (NullPointerException e) {
-            return "Exception Handled";
+            if (this.message.contains(null))
+                return "SAD";
+            else
+                return "HAPPY";
+        } catch (Exception e) {
+            if (message==null)
+                throw new MoodAnalyserException("Please Enter Valid Mood , Don't enter NUll",MoodAnalyserException.Exception_Type.NULL);
+            else
+                throw new MoodAnalyserException("Please Enter Valid Mood ,Don' keep Empty",MoodAnalyserException.Exception_Type.EMPTY);
         }
     }
 }
